8e4bd410a32885768b020846d30a374bb4d833df
02megha <119876636+02megha@users.noreply.github.com>
2022-12-14 20:30:02 +0530

    Add files via upload

---
+++ loop2.js
@@ -0,0 +1,24 @@
+for (let index = 0; index < 27; index = index+1)
+console.log("bhopal is a beutiful ")
+  
+console.log("looping compleated and come out of for bracket");
+
+
+for (let index = 0; index < 89; index = index+1)
+console.log("megha is agood girl and also she is cute and beautiful")
+console.log("looping compleated and come out of for bracket");
+
+
+
+for (let index = 0; index < 13; index = index+1)
+console.log("natural items are always good")
+
+console.log("looping compleated and come out of for bracket")
+
+//loops are used for the using the text to repeat itself or work on loop till the exit is 
+//defined there for loops just means the repeatition of the text //
+
+for (let index = 0; index < 100; index = index+1)
+console.log("i love you god i want to be with you always please be mine i just love him");
+
+console.log("looping compleated and come out of for bracket");
---
+++ practise.js
@@ -0,0 +1,13 @@
+let a = "i love my parents"
+console.log(a);
+
+ let b = "family love is important"
+ console.log(b);
+
+
+ let c = "stay happy always"
+ console.log(c);
+
+
+ let d = "keep urself happy"
+ console.log(c);
