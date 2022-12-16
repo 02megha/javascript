17a3a1887686895d969ffe5b6bef1d78aefdef6c
02megha <119876636+02megha@users.noreply.github.com>
2022-12-16 15:54:41 +0530

    Add files via upload

---
+++ practiseloop.js
@@ -0,0 +1,63 @@
+for (let index = 0; index < 89; index = index+1)
+console.log("megha is a good girl and also she is cute and beautiful")
+console.log("looping compleated and come out of for bracket");
+  
+
+for ( let i = 0 ; i < 40 ; i++)
+{
+    console.log( " electronics and communication department ")
+
+}
+
+
+for (var i = 9; i>0; i-=12){
+    console.log(i)
+}
+
+
+var arr = [10, 20 ,30 ,40 ,50, 60];
+for (let i = 0; i< arr.length; i++) {
+    console.log( arr[i]) 
+}
+
+
+var arr = [100, 200, 300, 400, 500, 600, 700, 800];
+for (let i = 0; i< arr.length; i++) {
+    console.log( arr[i]) 
+}
+
+
+let b =5;
+for(let index = 0; index < 100; index++){
+    console.log(b)
+    b = b+2
+}
+
+let a =1;
+for(let index = 0; index < 50; index++){
+    console.log(a)
+    a = a+5
+}
+ 
+
+let c = 2;
+for(let index = 3; index <80; index++){
+    console.log(c)
+    c=c*5;
+}
+
+
+let d = 7;
+for(let index = 3; index <80; index++){
+    console.log(d)
+    d=d*3;
+}
+
+
+
+let f = 7.4;
+for(let index = 4.9; index<50; index++){
+    console.log(f)
+    f=f*3.78;
+}
+
