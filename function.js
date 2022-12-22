f4301caec882e43427556c2024ee574c5c642df8
02megha <119876636+02megha@users.noreply.github.com>
2022-12-22 19:35:51 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,123 @@
+// Whenever we see a set of codes getting repeated or needed.
+// continuously then we define a function, which contains.
+//  that repeated codes inside it. Now whenever we want that codes.
+// we call that function and get those codes.
+
+// without functions
+
+// study items for ECE A
+
+
+console.log("electronics ");
+ console.log("digital electronics")
+ console.log("network analysis and synthesis");
+ console.log("engg physics");
+ console.log("data structure and analysis");
+
+
+
+ // // study items for ECE B
+
+ console.log("electronics ");
+ console.log("digital electronics");
+ console.log("network analysis and synthesis");
+ console.log("engg physics");
+ console.log("data structure and analysis");
+
+
+ // // study items for ECE C
+
+ console.log("electronics ");
+ console.log("digital electronics");
+ console.log("network analysis and synthesis");
+ console.log("engg physics");
+ console.log("data structure and analysis");
+
+ // // study items for ECE D
+
+ console.log("electronics ");
+ console.log("digital electronics");
+ console.log("network analysis and synthesis");
+ console.log("engg physics");
+ console.log("data structure and analysis");
+
+  // // study items for ECE E
+
+  console.log("electronics ");
+  console.log("digital electronics");
+  console.log("network analysis and synthesis");
+  console.log("engg physics");
+  console.log("data structure and analysis");
+ 
+ 
+
+// with functions (fat arrow);
+
+
+
+const electronicsfunction  = () => {
+    console.log("electronics");
+  };
+
+const digitalfunction= () => {
+    console.log("digital electronics");
+  };
+
+
+
+  const networkfunction = () => {
+    console.log("network analysis and synthesis");
+  };
+
+
+  const enggfunction  = () => {
+    console.log("engg physics");
+  };
+
+
+
+  const datafunction = () => {
+    console.log("data structure and analysis");
+  };
+
+  // ece a 
+  electronicsfunction();
+  digitalfunction();
+  networkfunction();
+  enggfunction();
+  datafunction();
+  console.log(" ECE A");
+
+  
+
+    // ece b
+    electronicsfunction();
+    digitalfunction();
+    networkfunction();
+    enggfunction();
+    datafunction();
+    console.log(" ECE B");
+
+ // ece c
+ electronicsfunction();
+ digitalfunction();
+ networkfunction();
+ enggfunction();
+ datafunction();
+ console.log(" ECE C");
+
+ // ece d
+ electronicsfunction();
+ digitalfunction();
+ networkfunction();
+ enggfunction();
+ datafunction();
+ console.log(" ECE D");
+
+ // ece e
+ electronicsfunction();
+ digitalfunction();
+ networkfunction();
+ enggfunction();
+ datafunction();
+ console.log(" ECE E");
