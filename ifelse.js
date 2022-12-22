d79998a6e16d2ef206c59df891f73595934f475f
02megha <119876636+02megha@users.noreply.github.com>
2022-12-22 19:25:33 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,28 @@
+//check whether a given number is even or odd
+//conditional if else conditions 
+// if else ka use krke even odd ka program likhna//
+let divisionOutput = 19 / 2;
+let difference = 19 - 2;
+let prodduct = 19 * 2;
+let sum = 19 + 2;
+let a = 19;
+
+if (a === 19) {
+  console.log(`a is 19`);
+} else if (a === 19) {
+  console.log(`a is 19`);
+} else if (a === 19) {
+  console.log(`a is 13`);
+} else {
+  console.log("no conditions matched");
+}
+
+if (14 % 2 === 0) {
+  console.log("14 is even");
+} else {
+  console.log("19 is odd");
+}
+
+
+
+14 % 2 === 0 ? console.log("14 is even") : console.log("14 is odd");
