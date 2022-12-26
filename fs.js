a2d82dd9fc4745382aa94fa93387ddcd58aa40ae
02megha <119876636+02megha@users.noreply.github.com>
2022-12-26 23:41:41 +0530

    Add files via upload

---
+++ fs.js
@@ -0,0 +1,39 @@
+const sub=(a,b) =>{
+let c= (a-b);
+return c
+
+};
+
+let sol1 =sub (5 ,6);
+console.log(sol1);
+letsol2 = sub (5 ,6);
+console.log(sol1);
+// addition
+const add=(a,b) =>{
+    let c=(a+b);
+    return c
+};
+let sol2=add(6,7);
+console.log(sol2);
+letsol2= add( 6,7);
+console.log(sol2);
+
+// multiplication 
+const multi=(a,b)=>{
+    let c=(a*b);
+    return c
+};
+ let sol3=multi(9,4);
+ console.log(sol3);
+ letsol3= multi(9.4);
+ console.log(sol3)
+
+ // division
+ const div=(a,b)=>{
+    let c=(a/b);
+    return c
+};
+ let sol4=div(4,2);
+ console.log(sol4);
+ letsol3= div(4,2);
+ console.log(sol4)
---
+++ fsp.js
@@ -0,0 +1,55 @@
+const sub=(a,b) =>{
+    let c= (a-b);
+    return c
+    
+    };
+    
+    let sol1 =sub (5 ,6);
+    console.log(sol1);
+    letsol2 = sub (5 ,6);
+    console.log(sol1);
+    // addition
+    const add=(a,b) =>{
+        let c=(a+b);
+        return c
+    };
+    let sol2=add(6,7);
+    console.log(sol2);
+    letsol2= add( 6,7);
+    console.log(sol2);
+    
+    // multiplication 
+    const multi=(a,b)=>{
+        let c=(a*b);
+        return c
+    };
+     let sol3=multi(9,4);
+     console.log(sol3);
+     letsol3= multi(9.4);
+     console.log(sol3)
+    
+     // division
+     const div=(a,b)=>{
+        let c=(a/b);
+        return c
+    };
+     let sol4=div(4,2);
+     console.log(sol4);
+     letsol3= div(4,2);
+     console.log(sol4);S
+
+
+
+     const func =( a,b,type)=>{
+        if (type==="sub"){
+            return a-b;}
+            else if (type==="add"){
+                return a+b;}
+            else if (type==="multi"){
+                return a*b;}
+                else if (type==="div"){
+                    return a/b;
+                }
+            
+        }
+    
